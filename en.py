14fa513d2fb06d08ed81e686796d3a8fea236e76
Sumi-king <nxcybar12@gmail.com>
2024-09-06 06:41:52 +0600

    Add files via upload

---
+++ en.py
@@ -0,0 +1,216 @@
+# --> Done Decoded By <⏤͟͞Nx-143?> <\> DecodeX+
+# --> Encryption </> OBF
+#FRIEND Nx-143
+
+import os
+import sys
+import requests
+import re
+import random
+import time
+import datetime
+import getpass
+from os import system as psl
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as bsn
+url = 'https://m.facebook.com/login.php'
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+        return None
+    None.system('clear')
+
+
+def sp(stri):
+    for letter in stri:
+        print(letter, end = '')
+        sys.stdout.flush()
+        return None
+
+os.system('xdg-open https://www.facebook.com/Sumi1251')
+os.system('clear')
+psl('rm -rf filer.txt')
+idd = []
+
+def logo():
+    print('\x1b[1;32m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(' 👍❤️TH3W N0NSTOP3R B0II MUSH9RIB L3G3ND SP33D FUCK3R D9RINDIW INXID3W❤️👍')
+    print('-----------------------------------------------------------------------')
+
+os.system('clear')
+logo2 = '\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\n\x1b[1;35;32m      😈 MUSH9RIB L3G3ND SP33D FUCK3R INXID3W 👿\n\n\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●  ___      ___  ____  ____    ________    __    __     __     \n    \x1b[0;92m\n    \x1b[1;31m███╗   ███╗██╗   ██╗███████╗██╗  ██╗██\n    \x1b[1;32m████╗ ████║██║   ██║██╔════╝██║  ██║██║\n    \x1b[1;33m██╔████╔██║██║   ██║███████╗███████║██║\n    \x1b[1;34m██║╚██╔╝██║██║   ██║╚════██║██╔══██║██║\n    \x1b[1;35m██║ ╚═╝ ██║╚██████╔╝███████║██║  ██║██║\n    \x1b[1;31m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝\n                                       \n\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[0;95m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\n \n\x1b[1;31m__________________________________________  \n\x1b[1;34;40m-----------------------------------------------\n\x1b[1;32mM9D3 BY  : 💝 MUSH9RIB CH9UDHRY 💝\n\x1b[1;33mFB ID    : ❣️https://www.facebook.com/profile.php?id=100087279292570❣️\n\x1b[1;35mY0UTUBE LINK  : 💚https://youtube.com/@MusharibChaudhry♥\n\x1b[1;31mWHATSAPP CONTACT NO   : 👿03196869049😈\n\x1b[1;31m-----------------------------------------------'
+print(logo2)
+
+def approval():
+    os.system('git pull')
+    uuid = str(os.geteuid()) + 'DS' + str(os.geteuid())
+    id = 'MUSH9RIB-TRICKER-' + ''.join(uuid)
+    os.system('clear')
+    print(logo2)
+    sp('\x1b[1;39m━▷ You Get Approved for Using Command  Paid Tool 350 Tk 1 Month \x1b[1;37m')
+    print('\n\x1b[1;39m━▷ Your Key :\x1b[36m ' + id)
+    print('\x1b[1;30m◑\x1b[1;31m◑\x1b[1;32m◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑\x1b[1;36m◑\x1b[1;37m◑\x1b[1;30m◑\x1b[1;31m◑\x1b[1;32m◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑\x1b[1;36m◑\x1b[1;37m◑\x1b[1;30m◑\x1b[1;31m◑\x1b[1;32m◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑\x1b[1;36m◑\x1b[1;37m◑\x1b[1;30m◑\x1b[1;31m◑\x1b[1;32m◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑\x1b[1;36m◑\x1b[1;37m◑\x1b[1;30m◑\x1b[1;31m◑\x1b[1;32m◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑\x1b[1;36m◑\x1b[1;37m◑◑\x1b[1;33m◑\x1b[1;34m◑\x1b[1;35m◑')
+    httpCaht = requests.get('https://github.com/Nx-143/Nx/blob/main/Approve.txt').text
+    if id in httpCaht:
+        sp('\n\x1b[1;39m━▷ Congrats You get approved successful And Enjoy')
+        msg = str(os.geteuid())
+        return None
+    None('\n\x1b[1;39m━▷ Your Key Not approved please  Say to Admin')
+    input('\n\nWHEN YOU BUY TOOL THEN ENTER PRESS')
+    tks = 'Hello%20Sir%20!%20Please%20Approved%20My%20Token%20The%20Token%20Is%20:%20' + id
+    (os.system('am start https://wa.me/+8801602331251?text=' + tks), approval())
+    time.sleep(2)
+    exit()
+    return None
+    if Exception:
+        e = None
+        print(e)
+        sp(' >> Unable Data From Server ')
+        time.sleep(2)
+        approval()
+        e = None
+        del e
+        return None
+    e = None
+    del e
+    sys.exit()
+
+approval()
+os.system('clear')
+print(logo2)
+attemps = 0
+#if attemps < 0x2DFDC184DL:
+    print('\x1b[1m\x1b[36m[+]  Enter Username \n')
+    print('\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+    username = input('\n\n\x1b[1;31;40mEnter Username: ')
+    print('\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+    print('\x1b[1m\x1b[36m[+] Enter Password \n')
+    print('\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●\x1b[1;37m๑۩♡۩๑\x1b[1;32m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●')
+    password = input('\x1b[1;32mEnter password: ')
+    if username == 's' and password == '0':
+        print('You have successfully logged in.')
+    print('Incorrect Password ')
+    attemps += 1
+psl('rm -rf filer.txt')
+idd = []
+
+def main():
+    psl('clear')
+    logo()
+    print('-------------------------------------------')
+    print('\x1b[1;97m  Put >>  Cookie, Delay, Chat/Inbox Link id, File Repeat, Hater Name, File ')
+    print('-------------------------------------------')
+    coki = input(' [+] Cookies : ')
+    cookies = {
+        'cookie': coki }
+    ch = requests.get('https://mbasic.facebook.com/', cookies = cookies)
+    if 'mbasic_logout_button' in ch.text:
+        pass
+    print(' \x1b[1;91mYour Account is on Checkpoint !!! \x1b[1;97m')
+    os.sys.exit()
+    print(' \x1b[1;92m Account Logged in Successfully\x1b[1;97m ')
+    print('-------------------------------------------')
+    delay = int(input(' [+] Delay : '))
+    print('-------------------------------------------')
+    lnk = input(' [+] Chat/Inbox Link id : ')
+    print('-------------------------------------------')
+    lim = int(input(' [+] File Repeat : '))
+    print('-------------------------------------------')
+    hater = input(' [+] Haters Name : ')
+    print('-------------------------------------------')
+    filee = input(' [+] File : ')
+    fileee = open(filee, 'r').read()
+    cpy(fileee, lim)
+    file = open('filer.txt', 'r').readlines()
+    idd.append(file)
+    crack = bsn(max_workers = 30)
+    psl('clear')
+    logo()
+    print('')
+    print(' \x1b[1;97m[+] Total messages : %s' % len(file))
+    print(' \x1b[1;97mYour Process Started in Background')
+    print('-------------------------------------------')
+    for mess in idd:
+        sm = '1'
+        if sm == '1':
+            crack.submit(msg, mess, coki, lnk, delay, hater)
+        os.sys.exit()
+        None(None, None)
+        return None
+        if not None:
+            pass
+
+
+def msg(mess, coki, lnk, delay, hater):
+    ses = requests.Session()
+    for msgs in mess:
+        time.sleep(delay)
+        timm = datetime.datetime.now()
+        (nm, ti) = str(timm).split(' ')
+        (tim, hff) = ti.split('.')
+        today = datetime.date.today()
+        (year, month, day) = str(today).split('-')
+        cookies = {
+            'cookie': coki }
+        g_url = 'https://d.facebook.com/messages/read/?tid=' + lnk
+        g_headers = 'Mozilla/5.0 (Linux; Android 11; TECNO CE7j) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Mobile Safari/537.36'
+        res1 = requests.get(url = g_url, cookies = cookies, headers = g_headers).text
+        j2 = re.search('name="jazoest" value="([^"]+)"', res1).group(1)
+        fb_dtsg = re.search('name="fb_dtsg" value="([^"]+)"', res1).group(1)
+        csid = re.search('name="csid" value="([^"]+)"', res1).group(1)
+        tids = re.search('name="tids" value="([^"]+)"', res1).group(1)
+        ses.headers.update({
+            'content-type': 'application/x-www-form-urlencoded' })
+        rose1 = sop(res1, 'html.parser')
+        rose = rose1.find('form', method = 'post')['action']
+        payload = {
+            'fb_dtsg': fb_dtsg,
+            'jazoest': j2,
+            'body': hater + ' ' + str(msgs),
+            'send': 'Send',
+            'tids': tids,
+            'wwwupp': 'C3',
+            'platform_xmd': '',
+            'referrer': '',
+            'ctype': '',
+            'cver': 'legacy',
+            'csid': csid }
+        host = 'https://d.facebook.com'
+        post = ses.post(url = host + rose, data = payload, cookies = cookies).text
+        print(' \x1b[1;97m[+] Time :\x1b[1;92m ' + str(tim))
+        print(' \x1b[1;97m[+] Date :\x1b[1;92m ' + day + '/' + month + '/' + year)
+        print(' \x1b[1;97m[+] Haterz : \x1b[1;92m' + hater)
+        print(' \x1b[1;97m[+] Message :\x1b[1;92m ' + hater + msgs + '\x1b[1;97m')
+        print('-------------------------------------------')
+        if requests.exceptions.ConnectionError:
+            'user-agent'
+            time.sleep(10)
+        if Exception:
+            e = '1'
+            e = None
+            del e
+            e = None
+            del e
+        loop += 1
+        return None
+        if requests.exceptions.ConnectionError:
+            'upgrade-insecure-requests'
+            time.sleep(10)
+            return None
+        if 'upgrade-insecure-requests':
+            e = '?1'
+            print(e)
+            e = None
+            del e
+            return None
+        e = '?1'
+        del e
+
+
+def cpy(fileee, lim):
+    for i in range(lim):
+        open('filer.txt', 'a').write(fileee + '\n')
+        return None
+
+main()
